e253f37cbcf4495ce6960721c4a159df7ab2a72a
AhmedAboelnaga <ahmedaboelnaga713@gmail.com>
2024-06-24 20:34:00 +0300

    added blocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: use_key_in_widget_constructors, camel_case_types, non_constant_identifier_names
 
 import 'package:basketball_points_counter/Views/home.dart';
+import 'package:basketball_points_counter/cubits/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(PointCounter());
@@ -10,9 +12,12 @@ void main() {
 class PointCounter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
     );
   }
 }
